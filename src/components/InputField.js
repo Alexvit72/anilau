a50d87d8adb99741efd4a748860571364fbaf585
Alexvit72 <aleksandrvitebsk@tut.by>
2020-11-29 16:51:57 +0300

    add control for form

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -3,8 +3,11 @@ import React from 'react';
 function InputField(props) {
 	return <div className={props.className}>
 		<span>{props.name}</span>
-		<input type={props.name == 'name' ? 'text' : props.name}
-			placeholder=''
+		<input
+			type={props.name == 'name' ? 'text' : props.name}
+			value={props.value}
+			placeholder={props.name == 'name' ? 'name' : props.name == 'email' ? '' : ''}
+			onChange={(event) => props.handleChange(event)}
 		/>
 		<img src={props.imgSrc} alt={props.name} />
 	</div>;
--- src/components/SubscrybeForm.js
+++ src/components/SubscrybeForm.js
@@ -1,29 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import InputField from './InputField';
 import Headline from './Headline';
 import ButtonMain from './ButtonMain';
-import user from '../images/formicons/user.png'
-import mail from '../images/formicons/mail.png'
-import phone from '../images/formicons/phone.png'
+import user from '../images/formicons/user.png';
+import mail from '../images/formicons/mail.png';
+import phone from '../images/formicons/phone.png';
 
 function SubscrybeForm(props) {
-	return <div className={props.className}>
+
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [phone, setPhone] = useState('');
+	const [checked, setChecked] = useState(false);
+
+	function handleChange(setFunc, event) {
+		setFunc(event.target.value);
+	}
+
+	return <form className={props.className}
+		onSubmit={(event) => event.preventDefault()}
+	>
 		<Headline className='Headline' text='Say Hello' punctuation='!' />
 		<span>and we will write you</span>
 		<div>
-			<InputField className='InputField' name='name' imgSrc={user} />
-			<InputField className='InputField' name='email' imgSrc={mail} />
-			<InputField className='InputField' name='phone' imgSrc={phone} />
+			<InputField className='InputField' name='name' imgSrc={user}
+				value={name} handleChange={(event) => handleChange(setName, event)}
+			/>
+			<InputField className='InputField' name='email' imgSrc={mail}
+				value={email} handleChange={(event) => handleChange(setEmail, event)}
+			/>
+			<InputField className='InputField' name='phone' imgSrc={phone}
+				value={phone} handleChange={(event) => handleChange(setPhone, event)}
+			/>
 		</div>
 		<div>
 			<ButtonMain className='ButtonMain' text='Send' />
 			<label>
-				<input type='checkbox' />
+				<input type='checkbox'
+					checked={checked}
+					onChange={() => setChecked(!checked)}
+				/>
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor</p>
 			</label>
 		</div>
+	</form>;
 
-	</div>;
 }
 
 export default SubscrybeForm;
