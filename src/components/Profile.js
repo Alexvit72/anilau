9078d3ecf799de0ead20a957ead89b0fd4609f80
Alexit72 <aleksandrvitebsk@tut.by>
2021-01-12 14:23:28 +0300

    update portraits of workers

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,12 +2,6 @@ import React from 'react';
 import './Profile.css';
 import Headline from './Headline';
 import ContLink from './ContLink';
-import port1 from '../images/portraits/port1.png';
-import port2 from '../images/portraits/port2.png';
-import port3 from '../images/portraits/port3.png';
-import port4 from '../images/portraits/port4.png';
-const portraits = [];
-portraits.push(port1, port2, port3, port4);
 
 function Profile(props) {
   const {fullName, position, contacts} = props.worker;
@@ -16,7 +10,7 @@ function Profile(props) {
     list.push(<ContLink key={field} id={field} contact={contacts[field]} className='ContLink' />);
   }
   return <div className={props.className}>
-    <img src={portraits[props.index]} alt={fullName} />
+    <img src={process.env.PUBLIC_URL + '/portraits/port' + (props.index + 1) + '.png'} alt={fullName} />
     <Headline className='Headline' text={fullName} punctuation='' />
     <p>{position}</p>
     <div>
